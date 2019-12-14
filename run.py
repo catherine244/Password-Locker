1a71b3ff182420194b5b3df80ec8e882e9d2b73b
catherine244 <ccayreen24@gmail.com>
2019-12-14 14:41:52 +0300

    added a pipe in  run.py

---
+++ run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.6
+from account import Account
+from credentials import Credentials
+
+def create_account(account_name,user_name,password,email):
+    '''
+    Function to create a new account
+    '''
+    new_account = Account(account_name,user_name,password,email)
+    return new_account
+
+def save_accounts(account):
+    '''
+    Function to save account
+    '''
+    account.save_account()    
+
+def del_account(account):
+    '''
+    Function to delete a account
+    '''
+    account.delete_account()    
