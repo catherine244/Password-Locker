6ba642360851888eafc0e400806d9963f6b39ea5
catherine244 <ccayreen24@gmail.com>
2019-12-14 09:10:19 +0300

    added def save account in account_test.py totest if the account object  is saved into the account list

--- account_test.py
+++ account_test.py
@@ -17,3 +17,19 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(self.new_account.user_name,"Nduku")
         self.assertEqual(self.new_account.password,"Cate1234")
         self.assertEqual(self.new_account.email,"catenduku995@gmail.com")
+        
+    def test_save_account(self):
+        '''
+        test_save_account test case to test if the account object is saved into
+         the account list
+        '''
+        self.new_account.save_account() # saving the new account
+        self.assertEqual(len(Account.account_list),1)  
+
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            Account.account_list = []    
+   
