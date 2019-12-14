a4238cbfee25779518cb84e0af2a955d6ed81e99
catherine244 <ccayreen24@gmail.com>
2019-12-14 09:13:21 +0300

    added a class account in account.py a class that generates new instances of users

---
+++ account.py
@@ -0,0 +1,11 @@
+class Account:
+    """
+    Class that generates new instances of users.
+    """
+    account_list = [] #empty account list
+
+    def __init__(self,account_name,user_name,password,email):
+        self.account_name = account_name
+        self.user_name = user_name
+        self.password = password
+        self.email = email
