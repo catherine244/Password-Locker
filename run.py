7711747993268a929a8942f3b6b6dd00eba200fc
catherine244 <ccayreen24@gmail.com>
2019-12-14 14:44:13 +0300

    added a def display accounts that returns all saved accounts  in  run.py

--- run.py
+++ run.py
@@ -20,3 +20,20 @@ def del_account(account):
     Function to delete a account
     '''
     account.delete_account()    
+def find_account(name):
+    '''
+    Function that finds a account by nane and returns the account
+    '''
+    return Account.find_by_name(name)    
+
+def check_existing_accounts(name):
+    '''
+    Function that check if an account exists with that name and return a Boolean
+    '''
+    return Account.account_exist(name)    
+
+def display_accounts():
+    '''
+    Function that returns all the saved accounts
+    '''
+    return Account.display_accounts()  
