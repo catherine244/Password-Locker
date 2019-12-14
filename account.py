7463836e1d512fc6bdaa50378d5c2ebfa0650a32
catherine244 <ccayreen24@gmail.com>
2019-12-14 10:29:54 +0300

    added decorators  in account.py

--- account.py
+++ account.py
@@ -26,4 +26,33 @@ class Account:
         delete_account method deletes a saved account from the account_list
         '''
 
-        Account.account_list.remove(self)    
+        Account.account_list.remove(self)  
+        
+        
+    @classmethod
+    def find_by_name(cls,name):
+        for account in cls.account_list:
+            if account.account_name == name:
+                return account    
+    
+
+    @classmethod
+    def account_exist(cls,name):
+        '''
+        Method that checks if a account exists from the account list.
+        Args:
+            name: Acc name to search if it exists
+        Returns :
+            Boolean: True or false depending if the account exists
+        '''
+        for account in cls.account_list:
+            if account.password == name:
+                    return account
+
+        return False
+    @classmethod
+    def display_accounts(cls):
+        '''
+        method that returns the account list
+        '''
+        return cls.account_list      
