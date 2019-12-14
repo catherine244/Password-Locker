df9426714408a6defc4b011b16240f72a2ac52f3
catherine244 <ccayreen24@gmail.com>
2019-12-14 12:47:07 +0300

    added  decorators in credentials.py file

--- credentials_test.py
+++ credentials_test.py
@@ -31,4 +31,61 @@ class TestCredentials(unittest.TestCase):
             '''
             tearDown method that does clean up after each test case has run.
             '''
-            Credentials.credentials_list = []        
+            Credentials.credentials_list = [] 
+
+    def test_save_multiple_credentials(self):
+            '''
+            test_save_multiple_account to check if we can save multiple account
+            objects to our account_list
+            '''
+            self.new_credentials.save_credentials()
+            test_credentials = Credentials("Test","user","abcd45678","test@user.com") # new account
+            test_credentials.save_credentials()
+            self.assertEqual(len(Credentials.credentials_list),2)
+            
+            
+            def test_delete_credentials(self):
+                '''
+            test_delete_account to test if we can remove an account from our account list
+            '''
+            self.new_credentials.save_credentials()
+            test_credentials = Credentials("Test","user","abcd45678","test@user.com") # account
+            test_credentials.save_credentials()
+
+            self.new_credentials.delete_credentials()# Deleting an account object
+            self.assertEqual(len(Credentials.credentials_list),1) 
+
+    def test_find_credentials_by_credentials_name(self):
+        '''
+        test to check if we can find an account by account_name and display information
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Test","user","abcd45678","test@user.com") # new account
+        test_credentials.save_credentials()
+
+        found_credentials = Credentials.find_by_name("Test")
+
+        self.assertEqual(found_credentials.email,test_credentials.email)  
+
+
+
+    def test_credentials_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the account.
+        '''
+
+        self.new_credentials.save_credentials()
+        test_credentials = Credentials("Test","user","abcd45678","test@user.com") # new account
+        test_credentials.save_credentials()
+
+        credentials_exists = Credentials.credentials_exist("abcd45678")
+        self.assertTrue(credentials_exists)    
+        
+                       
+
+
+if __name__ == '__main__':
+    unittest.main()    
+
+  
